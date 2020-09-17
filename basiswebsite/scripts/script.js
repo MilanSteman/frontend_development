868b1d453bb3adf49e5c462be4417dc3ea994baa
Milan <milan.steman@hva.nl>
2020-09-18 01:46:52 +0200

    Week 2
    Eerste styling op homepage gedaan. Top en bottom navigatie gemaakt en wat content toegevoegd op de homepage.

--- basiswebsite/scripts/script.js
+++ basiswebsite/scripts/script.js
@@ -1,1 +1,14 @@
 // JavaScript Document
+var navigation = document.querySelector('header nav');
+var content = document.querySelector('main');
+var topNav = navigation.offsetTop;
+
+window.addEventListener('scroll' , function(){
+    if (window.pageYOffset >= topNav) {
+        content.classList.add('fixedMargin')
+        navigation.classList.add('fixed')
+      } else {
+        content.classList.remove('fixedMargin')
+        navigation.classList.remove('fixed');
+      }
+});
