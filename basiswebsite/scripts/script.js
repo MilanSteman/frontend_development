1c38ad6be0c1ebc5e15860cf4b69e3e821273e09
Milan <milan.steman@hva.nl>
2020-10-09 10:53:46 +0200

    Voortgang week 3

--- basiswebsite/scripts/script.js
+++ basiswebsite/scripts/script.js
@@ -4,11 +4,22 @@ let content = document.querySelector('main');
 let topNav = navigation.offsetTop;
 
 let nieuwsAchtergrond = document.querySelector('.nieuws_achtergrond');
+let podcasts = document.querySelector('.podcasts')
+
 let voorpaginaNav = document.querySelector('header nav ul li:nth-of-type(1) a');
 let nieuwsAchtergrondNav = document.querySelector('header nav ul li:nth-of-type(2) a');
-let topNieuws = nieuwsAchtergrond.offsetTop;
+let podcastsNav = document.querySelector('header nav ul li:nth-of-type(4) a');
 
+console.log(podcasts)
 
+/**
+ * Vraagje voor morgen
+ * Nu addeventlistener met scroll maar smooth scroll werkt nu niet als je terugnavigeert via
+ * navbar, want je nav.scroll conflict er dan mee.
+ * 
+ * Misschien een fix is om alle a'tjes van de nav een click functie te geven
+ * met scroll. TODO <<<<<<<<<<<<
+ */
 window.addEventListener('scroll' , function(){
     if (window.pageYOffset >= topNav) {
         content.classList.add('fixedMargin')
@@ -18,13 +29,22 @@ window.addEventListener('scroll' , function(){
         navigation.classList.remove('fixed');
       }
 
-      if (window.pageYOffset <= topNieuws) {
+      if (window.pageYOffset <= nieuwsAchtergrond.offsetTop) {
         voorpaginaNav.classList.add('active');
         nieuwsAchtergrondNav.classList.remove('activeBlue');
         navigation.scroll({left: 0, behavior: 'smooth'});
-      } else if (window.pageYOffset >= topNieuws) {
+
+      } else if (window.pageYOffset >= nieuwsAchtergrond.offsetTop && window.pageYOffset <= podcasts.offsetTop) {
         voorpaginaNav.classList.remove('active');
         nieuwsAchtergrondNav.classList.add('activeBlue');
+        podcastsNav.classList.remove('activeBlue');
         navigation.scroll({left: nieuwsAchtergrondNav.offsetLeft - 15, behavior: 'smooth'});
+
+      } else if (window.pageYOffset >= podcasts.offsetTop) {
+        voorpaginaNav.classList.remove('active');
+        nieuwsAchtergrondNav.classList.remove('activeBlue');
+        podcastsNav.classList.add('activeBlue');
+        navigation.scroll({left: podcastsNav.offsetLeft - 15, behavior: 'smooth'});
       }
 });
+
